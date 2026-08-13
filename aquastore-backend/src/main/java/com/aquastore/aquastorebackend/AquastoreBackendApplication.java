947ec40bb5bcb2c8c713a6c0c8ccc3bdf0581b67
Pragati Sharma <sharma.pragati0908@gmail.com>
2026-08-13 11:54:09 +0530

    chore: initialize AquaStore backend

---
+++ aquastore-backend/src/main/java/com/aquastore/aquastorebackend/AquastoreBackendApplication.java
@@ -0,0 +1,14 @@
+package com.aquastore.aquastorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquastoreBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AquastoreBackendApplication.class, args);
+    }
+
+}
